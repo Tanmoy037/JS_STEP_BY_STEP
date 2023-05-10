bd56dc40f439a4f0c335efa6242a461e82e5027b
Tanmoy037 <tanmoysantra67@gmail.com>
2023-05-10 16:27:41 +0530

    first commit using js

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+
+let colour = ['red', 'green', 'blue'];
+console.log(colour[1]);
+
+
+function name(firstname , lastname){
+    console.log('Hello ' + firstname + ' ' + lastname);
+}
+name('Tanmoy' , 'Santra');
+
+// how to write if else in js
+myName = 'Sam';
+if (myName == 'Tanmoy') {
+    console.log('Hello Tanmoy');
+}else if (myName == 'Sam') {
+    console.log('Hello Tanmoy elif');
+}else {
+    console.log('Hello Tanmoy else');
+}          
+
+for (let i = 0; i <= 10; i++){
+    console.log(i);
+
+}
